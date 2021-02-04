1ead64cbd5cc7a84008eb5cae0e395614abb132e
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-02-04 20:40:51 +0530

    test assignment

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post("/create", User.create);
+router.post("/add/:userA/:userB", User.pending);
 router.get("/all", User.find);
 router.get("/friendRequests/:userA", User.getPendingFriendRequests);
 router.get("/friends/:userA", User.getAllFriends);
 router.get("/suggestions/:userA", User.getSuggestionForFriends);
-// post requests
-router.post("/create", User.create);
-router.post("/add/:userA/:userB", User.pending);
+
 
 module.exports = router;
